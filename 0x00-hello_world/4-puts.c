d5e0eed9329967ffdb3cb9309ec793a3275a000e
Ginabest <ukahgina@gmai>
2022-09-11 09:33:30 -0700

    Script that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+
+
+
 /**
- * main -Entry point, prints Programming is like building a multilingual puzzle
- * Return : Always 0 (Success)
- */
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+
 {
 
-	puts("Programming is like building a multilingual puzzle\n");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	Return 0;
+			return (0);
 
 }
