c73329b538ee950348023e56fcd2f5048c637aa1
Ginabest <ukahgina@gmai>
2022-09-16 09:19:23 -0700

    Prints positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,5 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
 
@@ -20,7 +18,6 @@ int main(void)
 
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
-
 		if (n > 0)
 			printf("%d is positive\n", n);
 		else if (n == 0)
--- 0x04-more_functions_nested_loops/README.md
+++
@@ -1,1 +0,0 @@
-Script to check for uppercase letter
